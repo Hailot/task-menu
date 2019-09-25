bdb0f0a73e6721e5269ec2adf5aef5bfb36cc679
Hailot <lordfe@kreakageklubben.org>
2019-09-25 12:43:56 +0200

    ide helper

--- app/Menu.php
+++ app/Menu.php
@@ -4,6 +4,25 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Menu
+ *
+ * @property int $id
+ * @property string $name
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Item[] $items
+ * @property-read int|null $items_count
+ * @property-read \Illuminate\Database\Eloquent\Model|\Eloquent $parentable
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Menu newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Menu newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Menu query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Menu whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Menu whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Menu whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Menu whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Menu extends Model
 {
     //
